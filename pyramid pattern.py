6fdf4d58ea74427f3180eb7107ae8c20c1361119
KULDEEP GAUTAM <118754536+kuldeeepp@users.noreply.github.com>
2022-12-07 10:41:44 +0530

    Add files via upload

---
+++ pyramid pattern.py
@@ -0,0 +1,23 @@
+s=20
+for i in range(1,6):
+    print(' '*s,end=' ')
+    for j in range(1,i+1):
+        print(j,end=' ')
+    print()
+    s-=1
+                                                                                                                    
+s=14
+for i in range(1,6):
+    print(' '*s,end=' ')
+    for j in range(1,i+1):
+        print("$",end=' ')
+    print()
+    s-=1
+                                                                                                                    
+s=24
+for i in range(1,6):
+    print(' '*s,end=' ')
+    for j in range(1,i+1):
+        print("$",end=' ')
+    print()
+    s-=1
